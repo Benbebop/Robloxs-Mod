8d940082305d102576d6d5ecf535b8491a51fc9f
Benbebop <78389958+Benbebop@users.noreply.github.com>
2021-03-09 09:10:05 -0700

    Update Util.lua

--- editor/Util.lua
+++ editor/Util.lua
@@ -1,1 +1,14 @@
+local joinUI = Instance.new("Configuration", script)
+joinUI.Name = "trackedUi"
 
+return setmetatable({}, {
+	__call = function(ui, addend)
+		if ui.class == "panel" and addend.class == "panel" then
+			
+		elseif ui.class == "joinPanel" and addend.class == "panel" then
+			
+		else
+			error("attempt to join " .. ui.class .. " with " .. addend.class)
+		end
+	end
+})
