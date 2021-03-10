8721c75365d8f65b3d67392a709bf71f67b307fa
Benbebop <78389958+Benbebop@users.noreply.github.com>
2021-03-09 17:45:51 -0700

    Update decompRes.lua

--- RBLXSDK/decompRes.lua
+++ RBLXSDK/decompRes.lua
@@ -1,5 +1,30 @@
 local http = game:GetService("HttpService")
 
+local configWidget = plugin:CreateDockWidgetPluginGui("tmpConfigWidget", DockWidgetPluginGuiInfo.new(Enum.InitialDockState.Float, true, true, 200, 100, 200, 100))
+configWidget.Title = "Convert Resource File"
+
+Instance.new("UIListLayout", configWidget)
+local tmp = Instance.new("TextLabel", configWidget)
+tmp.Name = "convertOffset"
+tmp.LayoutOrder = 0
+tmp.Text = "convertOffset"
+tmp.Size = UDim2.new(1, -20, 0, 20)
+local convertOffset = Instance.new("ImageButton", tmp)
+convertOffset.Size = UDim2.new(0, 14, 0, 14)
+convertOffset.Position = UDim2.new(1, -17, 0, 3)
+convertOffset.BackgroundColor3 = Color3.new(0, 0.6, 1)
+convertOffset.BorderColor3 = Color3.new(0.8, 0.8, 0.8)
+convertOffset:SetAttribute("value", true)
+convertOffset.MouseButton1Click:Connect(function()
+	local value = not convertOffset:GetAttribute("value")
+	if value then
+		convertOffset.BackgroundColor3 = Color3.new(0, 0.6, 1)
+	else
+		convertOffset.BackgroundColor3 = Color3.new(1, 1, 1)
+	end
+	convertOffset:SetAttribute("value", value)
+end)
+
 return function(str)
 	local lines = string.split(str, "\n")
 	for i,v in ipairs(lines) do
